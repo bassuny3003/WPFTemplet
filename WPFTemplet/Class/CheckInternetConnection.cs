a34b4d816afe38bca16fbd4d8b492f6d7dde47a4
Mohamed Bassuny <bassuny3003@users.noreply.github.com>
2020-07-05 18:12:52 +0200

    كلاس للتاد من وجود الانترنت
    عمل تأكيد لوجود الانترنت

---
+++ WPFTemplet/Class/CheckInternetConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+using System.Runtime.InteropServices;     // Check internet connection is available
+
+
+namespace WPFTemplet.Class
+{
+    class CheckInternetConnection
+    {
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int Description, int ReservedValue);
+
+        public bool InternetConnectionStatuse()
+        {
+            int Desc;
+            if (InternetGetConnectedState(out Desc, 0) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
