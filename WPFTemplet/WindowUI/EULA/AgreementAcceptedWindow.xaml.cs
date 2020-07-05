9a4a2570aa58e19e636f4c1851b3a1fbbd3344a6
Mohamed Bassuny <bassuny3003@users.noreply.github.com>
2020-07-05 18:19:32 +0200

    Create AgreementAcceptedWindow.xaml.cs
    انشاء صفحة تم الموافقه علي الاتفاقية

---
+++ WPFTemplet/WindowUI/EULA/AgreementAcceptedWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTemplet.WindowUI.EULA
+{
+    /// <summary>
+    /// Interaction logic for AgreementAcceptedWindow.xaml
+    /// </summary>
+    public partial class AgreementAcceptedWindow : Window
+    {
+        public AgreementAcceptedWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radArabic_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radEnglish_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
