f0f696b913336896ba5ff49212ae464609e26890
Mohamed Bassuny <bassuny3003@users.noreply.github.com>
2020-07-05 18:20:02 +0200

    Create UserAgreementWindow.xaml.cs
    انشاء صفحة برمجة الاتفاقية

---
+++ WPFTemplet/WindowUI/EULA/UserAgreementWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTemplet.WindowUI.EULA
+{
+    /// <summary>
+    /// Interaction logic for UserAgreementWindow.xaml
+    /// </summary>
+    public partial class UserAgreementWindow : Window
+    {
+        public UserAgreementWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void chkRememberChoice_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkRememberChoice_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void mnuAlwsTop_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuAlwsTop_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radArabic_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radEnglish_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDecline_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
