00164319b3e9559d65a65161ce2913c3c4629500
Mohamed Bassuny <bassuny3003@users.noreply.github.com>
2020-07-05 18:13:58 +0200

    Create DataBaseConnection.cs
    كلاس لعمل الاتصال بقاعدة البيانات و التاكد من وجود اتصال مع قاعدة البيانات

---
+++ WPFTemplet/Class/DataBaseConnection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+//using Finisar.SQLite;
+
+namespace WPFTemplet.Class
+{
+    class DataBaseConnection
+    {
+        //public SQLiteConnection GetConnection()
+        //{
+        //    return new SQLiteConnection("Data Source = " + Environment.CurrentDirectory + "\\Database\\SalesInvoiceTracker.db; Version = 3");
+        //}
+
+        //public bool CheckDBConnection()
+        //{
+        //    bool ConnectionStatus = false;
+
+        //    dbConnection StartConn = new dbConnection();
+        //    SQLiteConnection MyConnetion = StartConn.GetConnection();
+
+        //    MyConnetion.Open();
+
+        //    if (MyConnetion.State == ConnectionState.Open)
+        //    {
+        //        ConnectionStatus = true;
+        //    }
+
+        //    return ConnectionStatus;
+        //}
+    }
+}
