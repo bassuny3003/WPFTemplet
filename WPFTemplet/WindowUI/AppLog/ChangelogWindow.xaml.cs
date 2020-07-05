1fe8bf570c77d2e7089b630ed50c07bda30f7e72
Mohamed Bassuny <bassuny3003@users.noreply.github.com>
2020-07-05 18:18:32 +0200

    Create ChangelogWindow.xaml.cs
    انشاء برمجة صفحة تغيرات البرنامج

---
+++ WPFTemplet/WindowUI/AppLog/ChangelogWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTemplet.WindowUI.AppLog
+{
+    /// <summary>
+    /// Interaction logic for ChangelogWindow.xaml
+    /// </summary>
+    public partial class ChangelogWindow : Window
+    {
+        public ChangelogWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
