7868be353d271b36dcf4025b78d370068ca83fa5
Mohamed Bassuny <bassuny3003@users.noreply.github.com>
2020-08-14 20:13:22 +0200

    اصافة التغيرات اللازمة علي واجهة الموافقه علي الرخصة

--- WPFTemplet/WindowUI/EULA/AgreementAcceptedWindow.xaml.cs
+++ WPFTemplet/WindowUI/EULA/AgreementAcceptedWindow.xaml.cs
@@ -26,29 +26,61 @@ namespace WPFTemplet.WindowUI.EULA
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    return;
+                }
+                else
+                {
+                    brdrChanglogWindow.Opacity = 0.7;
 
+                    if (this.Cursor != Cursors.Wait)
+                    {
+                        Mouse.OverrideCursor = Cursors.SizeAll;
+                    }
+
+                    DragMove();
+                }
+            }
+
+            if (e.LeftButton == MouseButtonState.Released)
+            {
+                brdrChanglogWindow.Opacity = 1;
+
+                if (this.Cursor != Cursors.Wait)
+                {
+                    Mouse.OverrideCursor = Cursors.Arrow;
+                }
+            }
         }
 
         private void btnExit_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
 
         private void radArabic_Checked(object sender, RoutedEventArgs e)
         {
-
+            txtBxUserAgreement.FlowDirection = FlowDirection.RightToLeft;
+            txtBxUserAgreement.Text = Properties.Resources.EULAArabic;
         }
 
         private void radEnglish_Checked(object sender, RoutedEventArgs e)
         {
-
+            txtBxUserAgreement.FlowDirection = FlowDirection.LeftToRight;
+            txtBxUserAgreement.Text = Properties.Resources.EULAEnglish;
         }
 
         private void btnOk_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
 
+        private void btnExit_Click_1(object sender, RoutedEventArgs e)
+        {
 
+        }
     }
 }
