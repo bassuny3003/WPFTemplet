826021b82ddec2f6bcb5f1c120a47a95312e3fe0
Mohamed Bassuny <bassuny3003@users.noreply.github.com>
2020-08-14 20:12:14 +0200

    كلاس لتعريف الهراد و البوردة
    دي الكلاسات التي بتحدد فيها اجهزة معينة تشغل البرنامج نفسه

---
+++ WPFTemplet/Class/CheckHardDiskSerial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Management;
+
+namespace WPFTemplet.Class
+{
+    class CheckHardDiskSerial
+    {
+        string[] AllowedHardDiskSerialNo = new string[] 
+        { 
+            "4A9CC6FCB5C36230A820EF809A74DFE08D5F1CD0",                 /*My PC*/ 
+            "4A9CC6FCB5C36230A820EF809A74DFE08D5F1CD0FA141305",         /*My PC With My Flash */ 
+            "A2655514285B778F04618E86",                                 /* AlforsanGym */ 
+            "CCB1829B5300A5F050239DB04D689C604A8BE240"                  /*My Laptop*/ 
+        };
+
+        public bool GetHardDiskSerialNo()
+        {
+            bool DeviceStatus = false;
+
+            ManagementClass magmnt = new ManagementClass("Win32_LogicalDisk");
+            ManagementObjectCollection mcol = magmnt.GetInstances();
+
+            string CurrentHardDiskSerialNo = "";
+
+            foreach (ManagementObject strt in mcol)
+            {
+                CurrentHardDiskSerialNo += Convert.ToString(strt["VolumeSerialNumber"]);
+            }
+
+
+            if (AllowedHardDiskSerialNo.Contains(CurrentHardDiskSerialNo))
+            {
+                DeviceStatus = true;
+            }
+
+            return DeviceStatus;
+
+        }
+
+    }
+}
